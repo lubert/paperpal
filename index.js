c73632df5ef558f512f1d72d8a42822134e168d2
Lu Zhu <lu@plethora.com>
2018-06-27 00:39:37 +0000

    Implement basic functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const readline = require('readline');
 
+const got = require('got');
 const program = require('commander');
 
 let filePath;
@@ -10,7 +11,10 @@ let filePath;
 program
   .version('0.1.0')
   .arguments('[file]')
-  .option('-u, --url [URL]', 'Base URL prepended to paths')
+  .option('--cookie [COOKIE]', 'Cookie added to header')
+  .option('--exclude [REGEX]', 'Exclude')
+  .option('--url [URL]', 'Base URL prepended to paths')
+  .option('--no-check-certificate', 'Ignore SSL certificate errors')
   .action(function(file) {
     filePath = file;
   })
@@ -20,20 +24,50 @@ if (!process.argv.slice(2).length) {
   program.help();
 }
 
+if (!program.checkCertificate) process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const rl = readline.createInterface({
   input: fs.createReadStream(filePath)
 });
 
+const exclude = program.exclude ? new RegExp(program.exclude, 'g') : null;
 const regex = /(?<=GET )[^\s]*/g;
 
 rl.on('line', function(line) {
+  if (line.match(exclude)) return;
   const match = line.match(regex);
-  if (!match) {
-    console.log('Skipping', line);
-    return;
-  }
+  if (!match) return;
 
   let path = match[0];
   if (program.url) path = program.url + path;
-  console.log(path);
+  queueRequest(path);
 });
+
+let promise = Promise.resolve();
+
+function queueRequest(path) {
+  promise = promise.then(() => request(path));
+}
+
+const reqOpt = {};
+if (program.cookie) {
+  reqOpt.headers = {
+    cookie: program.cookie
+  };
+}
+
+function request(path) {
+  process.stdout.write(path + ' ');
+  const interval = setInterval(function() {
+    process.stdout.write('.');
+  }, 100);
+  return got(path, reqOpt)
+  .then(res => {
+    clearInterval(interval);
+    console.log(' ' + res.statusCode);
+  })
+  .catch(err => {
+    clearInterval(interval);
+    console.log(' ' + err.statusCode);
+  });
+}
